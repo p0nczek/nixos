da9db8ebf69ccaffd31ed6c6eeb8ed0280a9e472
shin <shin@nixos.local>
2026-05-30 06:29:40 +0200

    Add zsh plugins and p10k to nixos config

--- home.nix
+++ home.nix
@@ -7,12 +7,28 @@
 
   home.stateVersion = "25.11"; 
 
-  
+  # ZSH + OhMyZsh + p10k + zinit
+  programs.zsh = {
+    enable = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [ "git" "zsh-autosuggestions" "zsh-syntax-highlighting" ];
+    };
+  };
 
+  home.file.".zshrc".source = ./zshrc;
+  home.file.".p10k.zsh".source = ./zsh/p10k.zsh;
+  home.file.".local/share/zinit" = {
+    source = ./zsh-plugins/zinit;
+    recursive = true;
+    force = true;
+  };
+
+  # Noctalia
   programs.noctalia-shell = {
     enable = true;
     package = inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default.override { calendarSupport = true; };
-    
     settings = {
       bar = {
         position = "top";
@@ -21,13 +37,7 @@
     };
   };
 
-
-
-  
-
-  # Poprawne ścieżki – wszystko w folderze niri/
+  # Niri
   xdg.configFile."niri".source = ./niri;
   xdg.configFile."noctalia".source = ./noctalia;
-  home.file.".zshrc".source = ./zshrc;
-  home.file.".config/zsh".source = ./zsh-config;
 }
