d6bcc75a287b819b7d6f0393abcd429a5576de00
shin <shin@nixos.local>
2026-06-14 17:47:09 +0200

    noctaliaSettingsInRepo

--- configuration.nix
+++ configuration.nix
@@ -36,7 +36,7 @@ environment.sessionVariables = {
   #  SYSTEM LABEL
   # ============================================================================
   # Managed by the `nn` helper (updates label + git commit). Do not edit manually.
-  system.nixos.label = "writableConfigs";
+  system.nixos.label = "noctaliaSettingsInRepo";
 
   # ============================================================================
   #  HOME MANAGER (module integration)
