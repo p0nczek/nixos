59078e9340e12d720125687773cfc882adfc63ff
shin <shin@nixos.local>
2026-06-24 21:35:45 +0200

    jq

--- configuration.nix
+++ configuration.nix
@@ -36,7 +36,7 @@ environment.sessionVariables = {
   #  SYSTEM LABEL
   # ============================================================================
   # Managed by the `nn` helper (updates label + git commit). Do not edit manually.
-  system.nixos.label = "borderFix";
+  system.nixos.label = "jq";
 
   # ============================================================================
   #  HOME MANAGER (module integration)
@@ -326,6 +326,8 @@ boot.extraModprobeConfig = ''
     wayland
     libxkbcommon
     libGL
+
+	jq
 
     qmk
     pkgsCross.avr.buildPackages.gcc
