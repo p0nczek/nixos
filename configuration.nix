e91a4b0d9b29865ac7d8012a96ddc3850c64a32f
shin <shin@nixos.local>
2026-06-24 22:13:52 +0200

    usunieteMartweSkrypty

--- configuration.nix
+++ configuration.nix
@@ -36,7 +36,7 @@ environment.sessionVariables = {
   #  SYSTEM LABEL
   # ============================================================================
   # Managed by the `nn` helper (updates label + git commit). Do not edit manually.
-  system.nixos.label = "color";
+  system.nixos.label = "usunieteMartweSkrypty";
 
   # ============================================================================
   #  HOME MANAGER (module integration)
