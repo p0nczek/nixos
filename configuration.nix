3a68c77e6785a75e2419f2ca58c9b8500244d7a4
shin <shin@nixos.local>
2026-07-05 00:09:19 +0200

    test

--- configuration.nix
+++ configuration.nix
@@ -36,7 +36,7 @@ environment.sessionVariables = {
   #  SYSTEM LABEL
   # ============================================================================
   # Managed by the `nn` helper (updates label + git commit). Do not edit manually.
-  system.nixos.label = "zenBrowserNvidiaWaylandFix";
+  system.nixos.label = "test";
 
   # ============================================================================
   #  HOME MANAGER (module integration)
