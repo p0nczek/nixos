14d152fcbd8740b6f6dffd2c9d5b38f446c21abe
shin <shin@nixos.local>
2026-07-11 18:15:09 +0200

    test

--- configuration.nix
+++ configuration.nix
@@ -36,7 +36,7 @@ environment.sessionVariables = {
   #  SYSTEM LABEL
   # ============================================================================
   # Managed by the `nn` helper (updates label + git commit). Do not edit manually.
-  system.nixos.label = "nnfix_";
+  system.nixos.label = "test_";
 
   # ============================================================================
   #  HOME MANAGER (module integration)
